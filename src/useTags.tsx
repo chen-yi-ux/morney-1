e757fb7b030a33aef75650c230e565f8d65d9cd4
Cyer <1678447359@qq.com>
2021-11-01 15:20:24 +0800

    删除 tag

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -27,7 +27,13 @@ const useTags = () => {  // 封装一个自定义 Hook
     tagsClone.splice(index, 1, {id:id, name: obj.name});
     setTags(tagsClone);
   }
-  return {tags, setTags, findTag, updateTag, findTagIndex}
+  const deleteTag = (id: number) => {
+    const index = findTagIndex(id);
+    const tagsClone = JSON.parse(JSON.stringify(tags));
+    tagsClone.splice(index, 1);
+    setTags(tagsClone);
+  }
+  return {tags, setTags, findTag, updateTag, findTagIndex, deleteTag}
 }
 
 export {useTags};
--- src/views/Tag.tsx
+++ src/views/Tag.tsx
@@ -26,18 +26,14 @@ const InputWrapper = styled.div`
   background: white;
   padding: 0 16px;
   margin-top: 8px;
-`
+`;
+
 const Tag: React.FC = () => {
-  const {findTag, updateTag} = useTags();
+  const {findTag, updateTag, deleteTag} = useTags();
   let { id: idString } = useParams<Params>();
   const tag = findTag(parseInt(idString));
-  return (
-    <Layout>
-      <Topbar>
-        <Icon name="left"/>
-        <span>编辑标签</span>
-        <Icon/>
-      </Topbar>
+  const tagContent =(tag: {id:number, name: string}) => (
+    <div>
       <InputWrapper>
         <Input label="标签名" type="text" placeholder="标签名" value={tag.name}
                onChange={(e)=>{
@@ -49,9 +45,20 @@ const Tag: React.FC = () => {
         <Space/>
         <Space/>
         <Space/>
-        <Button>删除标签</Button>
+        <Button onClick={() => deleteTag(tag.id)}>删除标签</Button>
       </Center>
-    </Layout>
+    </div>
   )
+  return (
+      <Layout>
+        <Topbar>
+          <Icon name="left"/>
+          <span>编辑标签</span>
+          <Icon/>
+        </Topbar>
+        { tag ? tagContent(tag) : <Center>tag 不存在</Center>}
+      </Layout>
+    )
+
 }
 export {Tag};
