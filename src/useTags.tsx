3b4c8172be07eedcfa38ecb4476106ed3995740a
Cyer <1678447359@qq.com>
2021-11-01 16:12:01 +0800

    点击回退

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -6,34 +6,28 @@ const defaultTags = [
   {id: createId(), name: '食'},
   {id: createId(), name: '住'},
   {id: createId(), name: '行'}
-]
+];
 
 const useTags = () => {  // 封装一个自定义 Hook
-  const [tags, setTags] = useState<{id:number; name: string}[]>(defaultTags);
+  const [tags, setTags] = useState<{ id: number; name: string }[]>(defaultTags);
   const findTag = (id: number) => tags.filter(tag => tag.id === id)[0];
   const findTagIndex = (id: number) => {
     let result = -1;
-    for(let i=0; i<tags.length; i++){
-      if(tags[i].id === id){
+    for (let i = 0; i < tags.length; i++) {
+      if (tags[i].id === id) {
         result = i;
         break;
       }
     }
     return result;
-  }
-  const updateTag = (id: number, obj:{name: string}) => {
-    const index = findTagIndex(id);
-    const tagsClone = JSON.parse(JSON.stringify(tags))
-    tagsClone.splice(index, 1, {id:id, name: obj.name});
-    setTags(tagsClone);
-  }
+  };
+  const updateTag = (id: number, obj: { name: string }) => {
+    setTags(tags.map(tag => tag.id === id ? {id, name: obj.name} : tag));
+  };
   const deleteTag = (id: number) => {
-    const index = findTagIndex(id);
-    const tagsClone = JSON.parse(JSON.stringify(tags));
-    tagsClone.splice(index, 1);
-    setTags(tagsClone);
-  }
-  return {tags, setTags, findTag, updateTag, findTagIndex, deleteTag}
-}
+    setTags(tags.filter(tag => tag.id !== id));
+  };
+  return {tags, setTags, findTag, updateTag, findTagIndex, deleteTag};
+};
 
 export {useTags};
